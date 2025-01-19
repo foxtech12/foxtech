569b30b10eaaed903c294695818ab5118931ecec
bikash1129 <santrabikash29@gmail.com>
2025-01-19 16:33:13 +0530

    hello

--- client/src/component/Footer/Footer.jsx
+++ client/src/component/Footer/Footer.jsx
@@ -26,10 +26,10 @@ const Footer = () => {
 
       {/* Footer Content */}
       <div className="flex flex-col lg:flex-row justify-between items-start px-6 lg:px-24 py-12 w-full max-w-screen-xl mx-auto">
-        {/* Logo Section */}
-        <div className="mb-6 lg:mb-0">
-          <img src={logo} alt="Logo" className="w-40 h-auto lg:w-30 lg:mr-14" />
-        </div>
+      {/* Logo Section */}
+          <div className="mb-6 lg:mb-0 flex justify-center items-center w-full">
+            <img src={logo} alt="Logo" className="w-40 h-auto lg:w-30" />
+          </div>
 
         {/* Links Section */}
         <div className="flex flex-row w-full lg:w-2/3 justify-around">
@@ -50,29 +50,7 @@ const Footer = () => {
               <li className="text-xs sm:text-sm lg:text-base">
                 <Link to="/contact">Contact Us</Link>
               </li>
-              <li>
-                {/* Social Media Icons */}
-                <div className="flex justify-center sm:justify-start space-x-6 lg:space-x-8 mt-4">
-                  <FaFacebook
-                    className="text-blue-600 cursor-pointer hover:scale-110 transition-transform duration-200"
-                    size={26}
-                  />
-                  <FaLinkedin
-                    className="text-blue-800 cursor-pointer hover:scale-110 transition-transform duration-200"
-                    size={26}
-                  />
-                  <FaTwitter
-                    className="text-blue-400 cursor-pointer hover:scale-110 transition-transform duration-200"
-                    size={26}
-                  />
-                  <Link to="https://www.instagram.com/foxteach_offical/?hl=en">
-                    <FaInstagram
-                      className="text-pink-500 cursor-pointer hover:scale-110 transition-transform duration-200"
-                      size={26}
-                    />
-                  </Link>
-                </div>
-              </li>
+              
             </ul>
           </div>
         </div>
@@ -81,27 +59,8 @@ const Footer = () => {
           <h4 className="text-sm sm:text-base lg:text-lg font-semibold mb-4">
             Newsletter
           </h4>
-          <div className="flex flex-col sm:flex-row items-center">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              onChange={handleInput}
-              value={email}
-              className="w-full sm:w-3/4 lg:w-[80%] py-2 text-xs sm:text-sm lg:text-base border-b border-gray-400 outline-none bg-transparent mb-4 sm:mb-0 sm:mr-3"
-            />
-            <button
-              onClick={handleSubscribe}
-              className="text-xs sm:text-sm lg:text-base font-semibold border-b border-gray-700 text-gray-700 hover:text-black"
-            >
-              SUBSCRIBE
-            </button>
-          </div>
         </div>
       </div>
-
-      {/* Bottom Line */}
-      <div className="w-[90%] lg:w-[85%] h-px bg-gray-300 mx-auto"></div>
-
       {/* Copyright */}
       <div className="text-start ml-2 py-6 text-xs sm:text-sm lg:text-base text-gray-500">
         <p className="w-full lg:w-4/5 mx-auto">
