78d10bde6ce057ba32bfa5274934fe3fc57afdf1
bikash1129 <santrabikash29@gmail.com>
2025-01-19 16:52:45 +0530

    go

--- client/src/component/Footer/Footer.jsx
+++ client/src/component/Footer/Footer.jsx
@@ -12,11 +12,7 @@ const Footer = () => {
   };
 
   const handleSubscribe = () => {
-    if (email) {
       navigate(`/about?name=${email} - Footer`);
-    } else {
-      alert("Please enter a valid email.");
-    }
   };
 
   return (
@@ -24,8 +20,8 @@ const Footer = () => {
       {/* Top Line */}
       <div className="w-full h-px bg-gray-300"></div>
 
-      {/* Footer Content */}
-      <div className="flex flex-col lg:flex-row justify-between items-start px-6 lg:px-24 py-12 w-full max-w-screen-xl mx-auto">
+     {/* Footer Content */}
+     <div className="flex flex-col lg:flex-row justify-between items-start px-6 lg:px-24 py-12 w-full max-w-screen-xl mx-auto">
       {/* Logo Section */}
           <div className="mb-6 lg:mb-0 flex justify-center items-center w-full">
             <img src={logo} alt="Logo" className="w-40 h-auto lg:w-30" />
@@ -59,8 +55,20 @@ const Footer = () => {
           <h4 className="text-sm sm:text-base lg:text-lg font-semibold mb-4">
             Newsletter
           </h4>
+          <div className="flex flex-col sm:flex-row items-center">
+            <button
+              onClick={handleSubscribe}
+              className="text-xs sm:text-sm lg:text-base font-semibold border-b border-gray-700 text-gray-700 hover:text-black"
+            >
+              SUBSCRIBE
+            </button>
+          </div>
         </div>
       </div>
+
+      {/* Bottom Line */}
+      <div className="w-[90%] lg:w-[85%] h-px bg-gray-300 mx-auto"></div>
+
       {/* Copyright */}
       <div className="text-start ml-2 py-6 text-xs sm:text-sm lg:text-base text-gray-500">
         <p className="w-full lg:w-4/5 mx-auto">
